6de6c88879af7f542d5a86974893fc6475606d2b
Ana <anaflorisbelo@hotmail.com>
2024-09-13 16:19:10 -0300

    Modificação no exercicio2oo Livro

--- src/exercicio2oo/Livro.java
+++ src/exercicio2oo/Livro.java
@@ -1,30 +1,68 @@
 package exercicio2oo;
 
 public class Livro {
-    public String titulo;
-    public String autor;
-    public  String genero;
-    public boolean disponivel;
+    private String titulo;
+    private String autor;
+    private String genero;
+    private boolean disponivel;
 
-    public Livro(String titulo, String autor, String genero){
-        this.titulo=titulo;
-        this.autor=autor;
-        this.genero=genero;
-        this.disponivel=true;
+    // Construtor
+    public Livro(String titulo, String autor, String genero) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.disponivel = true;
     }
-    public void emprestar(){
-        if (disponivel){
-            disponivel = false;
+
+    // Getters e Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    // Métodos de emprestar e devolver
+    public void emprestar() {
+        if (this.disponivel) {
+            this.disponivel = false;
             System.out.println("Livro emprestado!");
-        }else{
-            System.out.println("Livro não disponivel!");
+        } else {
+            System.out.println("Livro não disponível!");
         }
     }
-    public void devolver(){
-        disponivel=true;
+
+    public void devolver() {
+        this.disponivel = true;
         System.out.println("Livro devolvido!");
     }
-    public boolean verificarDisponibilidade(){
-        return disponivel;
+
+    public boolean verificarDisponibilidade() {
+        return this.disponivel;
     }
 }
