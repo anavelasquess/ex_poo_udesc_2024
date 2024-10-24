25f3cb5ff4016ebaf6c6c491e51d934b88fa2012
Ana <anaflorisbelo@hotmail.com>
2024-10-24 15:22:39 -0300

    retangulo e testaRetangulo exercicio3oo

--- src/exercicio3oo/Retangulo.java
+++ src/exercicio3oo/Retangulo.java
@@ -4,17 +4,26 @@ public class Retangulo {
     private double largura;
     private double altura;
 
-    public Retangulo(double largura, double altura) {
-        this.largura = largura;
-        this.altura = altura;
+    public Retangulo() {
+        this.largura = 0;
+        this.altura = 0;
     }
 
+    public Retangulo(double largura, double altura) {
+        setLargura(largura);
+        setAltura(altura);
+    }
+
+    // Getters e Setters
     public double getLargura() {
         return largura;
     }
 
     public void setLargura(double largura) {
         this.largura = largura;
+        if (largura < 0) {
+            System.out.println("A largura não pode ser negativa: " + largura);
+        }
     }
 
     public double getAltura() {
@@ -23,9 +32,22 @@ public class Retangulo {
 
     public void setAltura(double altura) {
         this.altura = altura;
+        if (altura < 0) {
+            System.out.println("A altura não pode ser negativa: " + altura);
+        }
     }
 
     public double calcularArea() {
         return this.largura * this.altura;
+    }
+
+    @Override
+    public String toString() {
+        String areaStr = (largura >= 0 && altura >= 0) ? ", area=" + calcularArea() : "";
+        return "Retangulo{" +
+                "largura=" + largura +
+                ", altura=" + altura +
+                areaStr +
+                '}';
     }
 }
--- src/exercicio3oo/TestaRetangulo.java
+++ src/exercicio3oo/TestaRetangulo.java
@@ -1,10 +1,19 @@
 package exercicio3oo;
 
-import exercicio2oo.Retangulo;
-
 public class TestaRetangulo {
     public static void main(String[] args) {
-        Retangulo retangulo = new Retangulo(5, 5);
-        System.out.println("A área é: " + retangulo.calcularArea());
+        Retangulo retangulo1 = new Retangulo(5, 5);
+        System.out.println(retangulo1.toString());
+
+        Retangulo retangulo2 = new Retangulo(-3, 4); // Tentativa com largura negativa
+        System.out.println(retangulo2.toString());
+
+        Retangulo retangulo3 = new Retangulo(7, -10); // Tentativa com altura negativa
+        System.out.println(retangulo3.toString());
+
+        // Testando setters com valores negativos
+        retangulo2.setLargura(-10); // Tentativa de definir largura negativa
+        retangulo2.setAltura(7); // Definindo altura positiva
+        System.out.println(retangulo2.toString());
     }
 }
