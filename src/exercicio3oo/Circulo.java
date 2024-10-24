0c4111cc1294dbf5b013b9177dd364f304d9c8b3
Ana <anaflorisbelo@hotmail.com>
2024-10-24 15:32:49 -0300

    circulo e TestaCirculo  exercicio3oo

--- src/exercicio3oo/Circulo.java
+++ src/exercicio3oo/Circulo.java
@@ -2,9 +2,18 @@ package exercicio3oo;
 
 public class Circulo {
     private float raio;
+    private String nome;
 
-    public Circulo(float raio) {
-        this.raio = raio;
+    public Circulo() {
+        this.raio = 0;
+        this.nome = "";
+    }
+
+
+    public Circulo(float raio, String nome) {
+        this(); // Chama o construtor padrão
+        setRaio(raio);
+        setNome(nome);
     }
 
     public float getRaio() {
@@ -13,17 +22,39 @@ public class Circulo {
 
     public void setRaio(float raio) {
         this.raio = raio;
+        if (raio < 0) {
+            System.out.println("O raio não pode ser negativo: " + raio);
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public float calcularDiametro() {
-        return 2 * this.raio;
+        return (raio >= 0) ? 2 * this.raio : 0;
     }
 
     public float calcularPerimetro() {
-        return 2 * (float) Math.PI * this.raio;
+        return (raio >= 0) ? 2 * (float) Math.PI * this.raio : 0;
     }
 
     public float calcularArea() {
-        return (float) Math.PI * this.raio * this.raio;
+        return (raio >= 0) ? (float) Math.PI * this.raio * this.raio : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" +
+                "raio=" + raio +
+                ", nome='" + nome + '\'' +
+                ", diametro=" + calcularDiametro() +
+                ", perimetro=" + calcularPerimetro() +
+                ", area=" + calcularArea() +
+                '}';
     }
 }
--- src/exercicio3oo/TestaCirculo.java
+++ src/exercicio3oo/TestaCirculo.java
@@ -1,12 +1,11 @@
 package exercicio3oo;
 
-import exercicio3oo.Circulo;
-
 public class TestaCirculo {
     public static void main(String[] args) {
-        Circulo circulo = new Circulo(7);
-        System.out.println("O diâmetro é " + circulo.calcularDiametro());
-        System.out.println("O perímetro é " + circulo.calcularPerimetro());
-        System.out.println("A área é " + circulo.calcularArea());
+        Circulo circulo1 = new Circulo(7, "Círculo A");
+        System.out.println(circulo1.toString());
+
+        Circulo circulo2 = new Circulo(-5, "Círculo B"); // Tentativa com raio negativo
+        System.out.println(circulo2.toString());
     }
 }
