3ca75f06c3d68fd93690664fe89f7044b167bbf5
Ana <anaflorisbelo@hotmail.com>
2024-10-24 15:52:29 -0300

    contaBancaria e testaContaBancaria  exercicio3oo

--- src/exercicio3oo/ContaBancaria.java
+++ src/exercicio3oo/ContaBancaria.java
@@ -5,6 +5,14 @@ public class ContaBancaria {
     private String titular;
     private float saldo;
 
+    // Construtor padrão
+    public ContaBancaria() {
+        this.numero = "";
+        this.titular = "";
+        this.saldo = 0;
+    }
+
+    // Construtor com parâmetros
     public ContaBancaria(String numero, String titular) {
         this.numero = numero;
         this.titular = titular;
@@ -32,10 +40,18 @@ public class ContaBancaria {
     }
 
     public void depositar(float valor) {
+        if (valor < 0) {
+            System.out.println("O valor do depósito não pode ser negativo!");
+            return;
+        }
         this.saldo += valor;
     }
 
     public void sacar(float valor) {
+        if (valor < 0) {
+            System.out.println("O valor do saque não pode ser negativo!");
+            return;
+        }
         if (this.saldo >= valor) {
             this.saldo -= valor;
         } else {
@@ -45,5 +61,14 @@ public class ContaBancaria {
 
     public float consultarSaldo() {
         return this.saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaBancaria{" +
+                "numero='" + numero + '\'' +
+                ", titular='" + titular + '\'' +
+                ", saldo=" + saldo +
+                '}';
     }
 }
--- src/exercicio3oo/TestaContaBancaria.java
+++ src/exercicio3oo/TestaContaBancaria.java
@@ -1,21 +1,22 @@
 package exercicio3oo;
 
-import exercicio3oo.ContaBancaria;
-
 public class TestaContaBancaria {
     public static void main(String[] args) {
-        exercicio2oo.ContaBancaria contaMari = new exercicio2oo.ContaBancaria("2016", "Mari");
+        ContaBancaria contaMari = new ContaBancaria("2016", "Mari");
         contaMari.depositar(500);
         contaMari.sacar(100);
         System.out.println("Saldo de Mari é: " + contaMari.consultarSaldo());
         contaMari.sacar(500);
         System.out.println("O saldo de Mari é: " + contaMari.consultarSaldo());
 
-        exercicio3oo.ContaBancaria contaAna = new ContaBancaria("2024", "Ana");
+        ContaBancaria contaAna = new ContaBancaria("2024", "Ana");
         contaAna.depositar(500);
         contaAna.sacar(100);
         System.out.println("Saldo de Ana é: " + contaAna.consultarSaldo());
         contaAna.sacar(300);
         System.out.println("O saldo de Ana é: " + contaAna.consultarSaldo());
+
+        contaAna.depositar(-50); // Deve mostrar erro
+        contaAna.sacar(-100); // Deve mostrar erro
     }
 }
